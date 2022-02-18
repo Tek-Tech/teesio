cf22d6a0bd9ab2b0bb2487c4cd73e17728690cfa
dev0ps221 <seybaxscorpion@gmail.com>
2022-02-18 13:09:26 +0000

    TeeSio , TeeSioSocket ...!!

---
+++ TeeSio.js
@@ -0,0 +1,101 @@
+const { Ear } = require("@tek-tech/ears");
+
+const path = require('path')
+class TeeSio extends Ear{
+
+
+
+    getSocketClassPath(){
+        return path.join(__dirname,"TeeSioSocket.js")
+    }
+
+
+    isListening(){
+        return this.isListening
+    }
+
+    isReady(){
+        return this.ready
+    }
+
+    gotSio(){
+        return this.gotConfig('sio')
+    }
+
+    gotConfig(configname){
+        return this.config.hasOwnProperty(configname)
+    }
+
+    getConfig(configname){
+        return (this.gotConfig(configname)) ? this.config[configname] : null
+    }
+
+    gotIo(){
+        return this.io
+    }
+
+    setIo(){
+        this.io = this.getConfig('sio')()
+    }
+
+    init(){
+        if(this.gotSio()){
+            this.setIo()
+            this.setReady()
+        }
+        else{
+            console.log('sio object not found..')
+        }
+    }
+
+    onNewSocket(socket){
+        console.log('got new socket connection...',socket)
+    }
+
+    listen(){
+        if(this.gotSio()){
+            if(!this.gotIo()) this.setIo()
+            this.io.listen(this.gotConfig('usesTeeWeb')?this.getConfig('server').getInstance():this.getConfig('server'))
+            this.io.on(
+                'connection',socket=>{
+                    this.onNewSocket(socket)
+                }
+            )
+            this.isListening = true
+        }
+    }
+
+    assignData(data){
+        Object.keys(data).forEach(
+            configname=>{
+                this.config[configname] = data[configname]
+            }
+        )
+    }
+
+
+
+
+    constructor(data){
+        super()
+        this.isListening = false
+        this.io = null
+        this.config = {}
+        this.assignData(data)
+        this.init()
+        this.whenReady(
+            ()=>{
+                console.log("Teesio is ready")
+            }
+        )
+    }
+
+
+}
+
+
+
+
+
+
+module.exports = TeeSio
---
+++ TeeSioSocket.js
@@ -0,0 +1,17 @@
+if((typeof window)==undefined){
+    const { Ear } = require("@tek-tech/ears");
+}
+class TeeSioSocket extends Ear{
+
+
+    init(){
+
+    }
+
+    constructor(socket,isCli=true){
+
+    }
+}
+if(typeof module!==undefined){
+    module.exports = TeeSioSocket
+}
